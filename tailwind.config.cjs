81d1ea39790b8d1f7550e3f975705f56ca7a6615
Elizabeth Poff <elizabeth.poff@austin.utexas.edu>
2023-10-06 16:27:10 -0500

    Added 1883 and GT Sectra fonts to public folder and referenced in tailwind.config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        '1883-Sans': ["'1883-Sans'", 'sans-serif'],
+        'GT-Sectra': ["'GT-Sectra'", 'serif'],
+		},
 	},
 	plugins: [],
 }
